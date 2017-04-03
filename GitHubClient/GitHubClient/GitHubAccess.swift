3887eb9df3bc7375bb9942235ddc89a3758fc16d
Austin Rogers <contactALRogers@gmail.com>
2017-04-03 14:49:06 -0700

    completed lecture code

---
+++ GitHubClient/GitHubClient/GitHubAccess.swift
@@ -0,0 +1,12 @@
+//
+//  GitHubAccess.swift
+//  GitHubClient
+//
+//  Created by Kent Rogers on 4/3/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+import Foundation
+
+let gitHubClientID = "f6b27ec710ceb8d12ef8"
+let gitHubClientSecret = "d537ffa47711b71e9a39df548c427430e9cb47d0"
---
+++ GitHubClient/GitHubClient/GitHubAuthController.swift
@@ -0,0 +1,35 @@
+//
+//  GitHubAuthController.swift
+//  GitHubClient
+//
+//  Created by Kent Rogers on 4/3/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+import UIKit
+
+class GitHubAuthController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func printTokenPressed(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+        let parameters = ["scope" : "email,user"]
+        
+        GitHub.shared.oAuthRequestWith(parameters: parameters)
+        
+        
+    }
+    
+}
